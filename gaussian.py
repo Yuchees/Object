a9c451662a1c80f418770f70ce63a459f57ae6e1
Yu Che <41577806+yuchees@users.noreply.github.com>
2019-03-05 13:54:52 +0000

    Add two new functions for distributing and collecting files with array jobs.

--- gaussian.py
+++ gaussian.py
@@ -232,6 +232,8 @@ class GaussianInout:
                 # Checking the ending line
                 if not re.match(r' File| Normal', lines[-1]):
                     unfinished = self.check_target_folder + '/../unfinished'
+                    if not os.path.exists(unfinished):
+                        os.mkdir(unfinished)
                     shutil.move(path, unfinished)
                 else:
                     error_line = lines[-4:-3][0]
@@ -274,7 +276,56 @@ class GaussianInout:
                 input_file.writelines(input_data)
         print('Finished. Total time:{}'.format(datetime.now() - start))
 
+    def distributed_files(self, path, number):
+        """
+        Distributed files into sub folders that can be applied for array jobs on
+        barkla.
+
+        :param path: The root folder
+        :param number: The number of files in each sub-folders
+        :type path: str
+        :type number: int
+        :return: None
+        """
+        print('Starting...')
+        n, i = 0, 1
+        if not path.endswith('/'):
+            path = path + '/'
+        j = len(os.listdir(path)) % number
+        for file in os.listdir(path):
+            root_path = path + file
+            sub_array_path = path + str(i)
+            if not os.path.exists(sub_array_path):
+                os.mkdir(sub_array_path)
+            shutil.move(root_path, sub_array_path)
+            n += 1
+            if (n == number and i != 1) or (n == (number + j) and i == 1):
+                n = 0
+                i += 1
+        print('Finished!\n'
+              'Distributed into {} folders.'.format((i - 1)))
+
+    def undistributed_files(self, path):
+        """
+        Collecting all sub-folders files to their root folder.
+
+        :param path: The root folder
+        :type path: str
+        :return:
+        """
+        if not path.endswith('/'):
+            path = path + '/'
+        for folder in os.listdir(path):
+            file_list = os.listdir(path + str(folder))
+            for file in file_list:
+                target_path = '{}/{}/{}'.format(path, folder, file)
+                shutil.move(target_path, path)
+            os.removedirs(path + str(folder))
+        print('Finished!')
+
 
 if __name__ == '__main__':
     gauss_function = GaussianInout(method='PM7', mol='dyes', seq='dimer')
     gauss_function.info('all')
+    gauss_function.undistributed_files('../../Documents/test')
+    gauss_function.distributed_files('../../Documents/test', 10)
