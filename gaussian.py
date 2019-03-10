762f7051085f10dcdfd99180ee7222f7756f0bab
Yu Che <41577806+yuchees@users.noreply.github.com>
2019-03-10 22:13:43 +0000

    Redefining variable's name to make it readable. Add counting information for screening functions.

--- gaussian.py
+++ gaussian.py
@@ -10,39 +10,45 @@ import re
 from datetime import datetime
 
 
-# noinspection PyBroadException
 class GaussianInout:
     """
     Gaussian 16 input and output files preparation function.
     """
-    def __init__(self, method, mol, seq, path='../../Documents'):
+    def __init__(self, method, mol, seq, root_path='../../Documents'):
         """
         :param method: DFT method
         :param mol: Molecule name
         :param seq: Sequence type
-        :param path: The root path for input and output files
+        :param root_path: The root path for input and output files
         :type method: str
         :type mol: str
         :type seq: str
         """
         self.gauss_method = method
-        self.mol_group = '{}_{}'.format(mol, seq)
+        self.elements = {1: 'H', 6: 'C', 7: 'N', 8: 'O', 9: 'F', 15: 'P',
+                         16: 'S', 17: 'Cl', 35: 'Br', 53: 'I'}
+        self.mol_name = '{}_{}'.format(mol, seq)
         # Gaussian16 header and barkla bash template
         self.header = './header_{}'.format(method)
         self.bash = './bash_template'
         # Molecular coordinators
-        self.mol_folder = '{}/{}/{}'.format(path, mol, seq)
+        self.mol_folder = '{}/{}/{}'.format(root_path, mol, seq)
         # Gaussian16 input and output folder
-        self.input_folder = '{}/input_{}'.format(path, method)
-        self.output_folder = '{}/output_{}'.format(path, method)
+        self.input_folder = '{}/input_{}/{}'.format(
+            root_path, method, self.mol_name)
+        self.output_folder = '{}/output_{}/{}'.format(
+            root_path, method, self.mol_name)
         # Error and negative frequency folder
-        self.check_target_folder = ('{}/result'.format(self.output_folder))
+        self.origin_result_folder = ('{}/result'.format(self.output_folder))
         # Normal terminated results
-        self.normal_folder = self.output_folder + '/' + self.mol_group
-        self.chk_path = ('%Chk=/users/psyche/volatile/gaussian'
-                         '/chk/{}/{}/'.format(method, mol))
+        self.normal_result_folder = (
+                self.output_folder + '/{}_final'.format(self.mol_name)
+        )
+        self.chk_path = (
+            '%Chk=/users/psyche/volatile/gaussian/chk/{}/'.format(method)
+        )
 
-    def setup_target_folder(self, folder):
+    def setup_result_folder(self, folder):
         """
         Setting targeted folder path.
 
@@ -51,7 +57,7 @@ class GaussianInout:
         """
         if folder.endswith('/'):
             folder = folder[:-1]
-        self.check_target_folder = folder
+        self.origin_result_folder = folder
 
     def setup_chk_path(self, chk_path):
         """
@@ -74,23 +80,23 @@ class GaussianInout:
             print('Gaussian16 {}'.format(self.gauss_method))
         if info in ['all', 'input']:
             print(
-                'Header template: {}\n'
-                'Checkpoint line: {}\n'
-                'Molecular:       {}\n'
-                'Gaussian input:  {}/{}'.format(
+                'Header template:    {}\n'
+                'Checkpoint line:    {}\n'
+                'Molecular folder:   {}\n'
+                'Gaussian input:     {}/{}'.format(
                     self.header,
                     self.chk_path,
                     self.mol_folder,
                     self.input_folder,
-                    self.mol_group
+                    self.mol_name
                 )
             )
         if info in ['all', 'neg', 'error']:
             print(
-                'Targeted folder: {}\n'
-                'Normal output:   {}'.format(
-                    self.check_target_folder,
-                    self.normal_folder
+                'HPC results folder: {}\n'
+                'Normal output:      {}'.format(
+                    self.origin_result_folder,
+                    self.normal_result_folder
                 )
             )
         if info in ['all', 'error_input']:
@@ -98,8 +104,8 @@ class GaussianInout:
                 'Error input folder: {}\n'
                 'Error check folder: {}'.format(
                     (self.input_folder + '/' +
-                     self.check_target_folder.split('/')[-1]),
-                    self.check_target_folder
+                     self.origin_result_folder.split('/')[-1]),
+                    self.origin_result_folder
                 )
             )
         if info not in ['all', 'input', 'neg', 'error', 'error_input']:
@@ -108,7 +114,7 @@ class GaussianInout:
                 'error_input.'
             )
 
-    def prep_input(self):
+    def prep_input(self, geom):
         """
         Generate gaussian input files.\n
         All chemical files must be stored under self.check_target_folder
@@ -122,96 +128,72 @@ class GaussianInout:
         print('Processing...')
         start = datetime.now()
         # Create folders for origin Gaussian input files
-        input_origin_folder = self.input_folder + '/' + self.mol_group
+        input_origin_folder = self.input_folder + '/{}'.format(self.mol_name)
         if not os.path.exists(self.input_folder):
             os.mkdir(self.input_folder)
         if not os.path.exists(input_origin_folder):
-                os.mkdir(input_origin_folder)
+            os.mkdir(input_origin_folder)
         with open(self.header, 'r') as header:
-            template = tuple(header.readlines())
+            template = header.readlines()
         # Generate Gaussian input data for all molecules
         for file in os.listdir(self.mol_folder):
-            input_data = list(template)
+            input_data = template.copy()
             # Get the molecular name
             name = file.split('.')[0]
             # Edit the checkpoint line
-            input_data[2] = self.chk_path + name + '.chk\n'
+            for i in range(len(input_data)):
+                if input_data[i].startswith('%Chk'):
+                    input_data[i] = self.chk_path + '{}.chk\n'.format(name)
             # Read molecule file
-            molecular_file = self.mol_folder + '/' + file
-            with open(molecular_file, 'r') as data:
-                # Get all atoms coordinates
-                # For mol format
-                if file.endswith('.mol'):
-                    for line in data:
-                        try:
-                            if line[35] == '0':
-                                coordinates = '{}   {}\n'.format(
-                                    line[31:33],
-                                    line[1:30]
-                                )
-                                input_data.append(coordinates)
-                        except:
-                            pass
-                # For xyz format
-                elif file.endswith('.xyz'):
-                    for line in data:
-                        try:
-                            if line[0].isalpha():
-                                input_data.append(line)
-                        except:
-                            pass
-                else:
-                    print('Waring!\n{} is not a MOL or XYZ format file!'.format(
-                        file))
-                    break
-            # Adding terminate line
-            input_data.append('\n')
+            if geom == 'local':
+                molecular_file = self.mol_folder + '/' + file
+                with open(molecular_file, 'r') as data:
+                    # Get all atoms coordinates
+                    # For mol format
+                    if file.endswith('.mol'):
+                        for line in data:
+                            segments = re.split(r'\s+', line)
+                            try:
+                                if segments[4] in self.elements.values():
+                                    coord_line = '{}   {}  {}  {}'.format(
+                                        segments[4],
+                                        segments[1],
+                                        segments[2],
+                                        segments[3]
+                                    )
+                                    input_data.append(coord_line)
+                            except IndexError:
+                                pass
+                    # For xyz format
+                    elif file.endswith('.xyz'):
+                        for line in data:
+                            try:
+                                if line[0].isalpha():
+                                    input_data.append(line)
+                            except IndexError:
+                                pass
+                    else:
+                        print('Waring!\n'
+                              '{} is not MOL or XYZ format!'.format(file))
+                        break
+                # Adding terminate line
+                input_data.append('\n')
+            elif geom == 'chk':
+                geom_line = False
+                for line in input_data:
+                    if line.startswith('# Geom'):
+                        geom_line = True
+                if not geom_line:
+                    input_data.insert(4, '# Geom=Checkpoint Guess=Read')
+            else:
+                print('Error! Geom must be local or chk.')
             # Writing data into a gjf file
             input_path = '{}/{}.gjf'.format(input_origin_folder, name)
             with open(input_path, 'w') as input_file:
                 input_file.writelines(input_data)
         print('Finished. Total time:{}'.format(datetime.now() - start))
 
-    def check_freq(self):
-        """
-        Checking the frequency information and distributing negative frequency
-        output files into 'neg_freq' folder.
-
-        :return: None
-        """
-        print('Targeted folder: {}'.format(self.check_target_folder))
-        start = datetime.now()
-        for file in os.listdir(self.check_target_folder):
-            if not file.endswith('.out'):
-                print('Error!\n{} is not a Gaussian out file!'.format(file))
-                break
-            path = self.check_target_folder + '/' + file
-            with open(path, 'r') as gauss_out:
-                for line in gauss_out:
-                    # Checking the frequencies
-                    if line.startswith(' Frequencies'):
-                        data = re.split(r'\s+', line)
-                        # Normal terminated jobs
-                        if float(data[3]) > 0:
-                            if not os.path.exists(self.normal_folder):
-                                os.mkdir(self.normal_folder)
-                                print('The final out files: {}'.format(
-                                    self.normal_folder))
-                            # Move to the final folder
-                            shutil.move(path, self.normal_folder)
-                            break
-                        # Negative frequencies
-                        elif float(data[3]) < 0:
-                            check_out_folder = self.output_folder + '/neg_freq'
-                            if not os.path.exists(check_out_folder):
-                                os.mkdir(check_out_folder)
-                                print(check_out_folder)
-                            # Move to negative frequencies folder
-                            shutil.move(path, check_out_folder)
-                            break
-        print('Finished. Total time:{}'.format(datetime.now() - start))
-
-    def check_error(self):
+    def error_screening(self):
         """
         Checking the output files and distributing error files into different
         folder.\n
@@ -220,21 +202,23 @@ class GaussianInout:
         :return: None
         """
         # Checking the error for output files
-        print('Targeted folder: {}'.format(self.check_target_folder))
+        print('Targeted folder: {}'.format(self.origin_result_folder))
         start = datetime.now()
-        for file in os.listdir(self.check_target_folder):
+        i, j = 0, 0
+        for file in os.listdir(self.origin_result_folder):
             if not file.endswith('.out'):
                 print('Error!\n{} is not a Gaussian out file!'.format(file))
                 break
-            path = self.check_target_folder + '/' + file
+            path = self.origin_result_folder + '/' + file
             with open(path, 'r') as gauss_out:
                 lines = gauss_out.readlines()
                 # Checking the ending line
                 if not re.match(r' File| Normal', lines[-1]):
-                    unfinished = self.check_target_folder + '/../unfinished'
+                    unfinished = self.origin_result_folder + '/../unfinished'
                     if not os.path.exists(unfinished):
                         os.mkdir(unfinished)
                     shutil.move(path, unfinished)
+                    i += 1
                 else:
                     error_line = lines[-4:-3][0]
                 # Checking the error indicator
@@ -242,37 +226,92 @@ class GaussianInout:
                         error = re.split(r'[/.]', error_line)[-3][1:]
                         # Creating a new folder for different
                         # categories of error
-                        new_path = self.output_folder + '/error_' + error
-                        if not os.path.exists(new_path):
-                            os.mkdir(new_path)
-                            print(new_path)
-                        shutil.move(path, new_path)
-        print('Finished. Total time:{}'.format(datetime.now() - start))
+                        error_folder = self.output_folder + '/error_' + error
+                        if not os.path.exists(error_folder):
+                            os.mkdir(error_folder)
+                            print(error_folder)
+                        shutil.move(path, error_folder)
+                        j += 1
+        print(
+            'Finished.\n'
+            'Normal results:            {}\n'
+            'Negative frequency result: {}\n'
+            'Total time:{}'.format(i, j, (datetime.now() - start))
+        )
 
-    def error_or_freq_input(self):
+    def neg_freq_screening(self):
+        """
+        Checking the frequency information and distributing negative frequency
+        output files into 'neg_freq' folder.
+
+        :return: None
+        """
+        print('Targeted folder: {}'.format(self.origin_result_folder))
+        start = datetime.now()
+        i, j = 0, 0
+        for file in os.listdir(self.origin_result_folder):
+            if not file.endswith('.out'):
+                print('Error!\n{} is not a Gaussian out file!'.format(file))
+                break
+            path = self.origin_result_folder + '/' + file
+            with open(path, 'r') as gauss_out:
+                for line in gauss_out:
+                    # Checking the frequencies
+                    if line.startswith(' Frequencies'):
+                        data = re.split(r'\s+', line)
+                        # Normal terminated jobs
+                        if float(data[3]) > 0:
+                            if not os.path.exists(self.normal_result_folder):
+                                os.mkdir(self.normal_result_folder)
+                            shutil.move(path, self.normal_result_folder)
+                            i += 1
+                            break
+                        # Negative frequencies
+                        elif float(data[3]) < 0:
+                            error_folder = self.output_folder + '/neg_freq'
+                            if not os.path.exists(error_folder):
+                                os.mkdir(error_folder)
+                            shutil.move(path, error_folder)
+                            j += 1
+                            break
+        print(
+            'Finished.\n'
+            'Normal results:            {}\n'
+            'Negative frequency result: {}\n'
+            'Total time:{}'.format(i, j, (datetime.now() - start))
+        )
+
+    def prep_error_input(self, error):
         """
         Generating input files for error and negative frequency results.\n
         Using prepared header information.
 
         :return: None
         """
-        print('Targeted folder: {}'.format(self.check_target_folder))
+        error_folder = self.output_folder + '/{}'.format(error)
+        print('Targeted folder: {}'.format(error_folder))
         start = datetime.now()
-        error_list = os.listdir(self.check_target_folder)
+        error_list = os.listdir(error_folder)
         with open(self.header, 'r') as header:
-            template = tuple(header.readlines())
+            template = header.readlines()
         for file in error_list:
-            input_data = list(template)
+            input_data = template.copy()
             name = file.split('.')[0]
+            geom_line = False
             # Edit checkpoint line
-            input_data[2] = self.chk_path + name + '.chk\n'
+            for i in range(len(input_data)):
+                if input_data[i].startswith('%Chk'):
+                    input_data[i] = self.chk_path + '{}.chk\n'.format(name)
+                elif input_data[i].startswith('# Geom'):
+                    geom_line = True
+            if not geom_line:
+                input_data.insert(4, '# Geom=Checkpoint Guess=Read')
             # Creating folder and writing the input files
-            input_folder_error = (self.input_folder + '/' +
-                                  self.check_target_folder.split('/')[-1])
-            if not os.path.exists(input_folder_error):
-                os.mkdir(input_folder_error)
-            input_file_path = input_folder_error + '/{}.gjf'.format(name)
-            with open(input_file_path, 'w') as input_file:
+            error_input_folder = (self.input_folder + '/{}'.format(error))
+            if not os.path.exists(error_input_folder):
+                os.mkdir(error_input_folder)
+            error_input_file = error_input_folder + '/{}.gjf'.format(name)
+            with open(error_input_file, 'w') as input_file:
                 input_file.writelines(input_data)
         print('Finished. Total time:{}'.format(datetime.now() - start))
 
@@ -325,7 +364,7 @@ class GaussianInout:
 
 
 if __name__ == '__main__':
-    gauss_function = GaussianInout(method='PM7', mol='dyes', seq='dimer')
+    gauss_function = GaussianInout(method='PM7_opt', mol='dyes', seq='dimer')
     gauss_function.info('all')
     gauss_function.undistributed_files('../../Documents/test')
     gauss_function.distributed_files('../../Documents/test', 10)
