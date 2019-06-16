470d78645760305b7779bdf2e5472a800f76c214
yu <Y.Che@liverpool.ac.uk>
2019-06-16 17:06:38 +0100

    New interactive plot

---
+++ interactive_plot.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: Yu Che
+"""
+import dash
+import flask
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+
+df = pd.read_csv('../SMP_project/tsne_data.csv')
+axis_template = dict(
+    showgrid=False,
+    zeroline=False,
+    showline=False,
+    showticklabels=False,
+)
+app.layout = html.Div([
+    html.Div(
+        dcc.Graph(
+            id='clickable_plot',
+            figure={
+                'data': [dict(
+                    x=df.loc[:, 'pos0'],
+                    y=df.loc[:, 'pos1'],
+                    text=df.loc[:, 'Molecules'],
+                    mode='markers',
+                    marker=dict(
+                        size=5,
+                        colorbar=dict(
+                            thicknessmode='pixels',
+                            thickness=20,
+                            title='HER Ave umol/h'
+                        ),
+                        color=df.loc[:, 'HER Ave umol/h'],
+                        colorscale='RdBu',
+                        showscale=True
+                    )
+                )],
+                'layout': go.Layout(
+                    clickmode='event+select',
+                    title='SMP_TSNE_map',
+                    hovermode='closest',
+                    xaxis=axis_template,
+                    yaxis=axis_template,
+                    showlegend=False,
+                    width=960,
+                    height=540
+                )
+            })
+    ),
+    html.Div([
+        html.Div(id='hover_molecule'),
+        html.Div(id='selected_molecule')]
+    )
+])
+
+
+def images_component(data):
+    image_name = 'template_image'
+    html_images = []
+
+    def html_image(name):
+        one_image = html.Img(
+            src='https://res.cloudinary.com/yucheimages/image/upload'
+                '/v1560534817/molecular_images/{}.png'.format(name),
+            style={'width': '400px', 'height': '300px'}
+        )
+        return one_image
+
+    try:
+        number_molecules = len(data['points'])
+        for i in range(number_molecules):
+            index = int(data['points'][i]['pointIndex'])
+            image_name = df.loc[index, 'Molecules']
+            html_images.append(html_image(image_name))
+    except TypeError:
+        html_images.append(html_image(image_name))
+    return html.Div(html_images)
+
+
+@app.callback(dash.dependencies.Output('hover_molecule', 'children'),
+              [dash.dependencies.Input('clickable_plot', 'hoverData')])
+def display_hover_image(hoverData):
+    return images_component(hoverData)
+
+
+@app.callback(dash.dependencies.Output('selected_molecule', 'children'),
+              [dash.dependencies.Input('clickable_plot', 'selectedData')])
+def display_selected_data(selectedData):
+    return images_component(selectedData)
+
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', prot=8050)
--- vae/vae_models.py
+++ vae/vae_models.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Variational auto encoder models
+Variational auto encoder models and properties prediction
 @author: Yu Che
 """
 from keras import layers, Input, Model, metrics
@@ -11,10 +11,10 @@ from keras import backend as K
 def encoder(params):
     encoder_input = Input(shape=params['input_shape'], name='encoder_input')
     x = layers.Conv1D(
-        filters=params['conv_filters'],
-        kernel_size=params['conv_kernel_size'],
-        activation=params['conv_activation'],
-        padding=params['conv_padding'],
+        filters=params['conv_filters'][0],
+        kernel_size=params['conv_kernel_size'][0],
+        activation=params['conv_activation'][0],
+        padding=params['conv_padding'][0],
         name='encoder_conv0'
     )(encoder_input)
     if params['conv_batch_norm']:
@@ -22,10 +22,10 @@ def encoder(params):
     if params['num_conv'] > 1:
         for i in range(1, params['num_conv']):
             x = layers.Conv1D(
-                filters=params['conv_filters'],
-                kernel_size=params['conv_kernel_size'],
-                activation=params['conv_activation'],
-                padding=params['conv_padding'],
+                filters=params['conv_filters'][i],
+                kernel_size=params['conv_kernel_size'][i],
+                activation=params['conv_activation'][i],
+                padding=params['conv_padding'][i],
                 name='encoder_conv{}'.format(i)
             )(x)
             if params['conv_batch_norm']:
@@ -48,6 +48,7 @@ def encoder(params):
             shape=(K.shape(mean)[0], params['latent_dim']), mean=0., stddev=1.
         )
         return mean + K.exp(0.5 * log_var) * epsilon
+
     z_sampling = layers.Lambda(sampling, name='vae_layer')([z_mean, z_log_var])
     encoder_model = Model(
         encoder_input, [z_mean, z_log_var, z_sampling],
@@ -61,20 +62,20 @@ def decoder(params):
     z = layers.Dense(
         units=params['middle_dim'],
         activation=params['middle_activation'],
-        name='decoder_dense'
+        name='decoder_middle'
     )(decoder_input)
     z = layers.RepeatVector(n=params['input_shape'][0])(z)
     output = layers.GRU(
-        units=params['gru_dim'],
-        activation=params['gru_activation'],
+        units=params['gru_dim'][0],
+        activation=params['gru_activation'][0],
         return_sequences=True,
         name='decoder_gru0'
     )(z)
     if params['num_gru'] > 1:
         for i in range(1, params['num_gru']):
             output = layers.GRU(
-                units=params['gru_dim'],
-                activation=params['gru_activation'],
+                units=params['gru_dim'][i],
+                activation=params['gru_activation'][i],
                 return_sequences=True,
                 name='decoder_gru{}'.format(i)
             )(output)
@@ -83,8 +84,29 @@ def decoder(params):
 
 
 def vae_loss(z_mean, z_log_var, x, y):
-    reconstruction_loss = metrics.binary_crossentropy(x, y)
+    reconstruction_loss = metrics.binary_crossentropy(
+        K.flatten(x),
+        K.flatten(y)
+    )
     kl_loss = -0.5 * K.mean(
         1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1
     )
     return K.mean(reconstruction_loss + kl_loss)
+
+
+def property_predication(params):
+    prop_input = Input(shape=params['latent_dim'], name='prediction_input')
+    x = layers.Dense(
+        units=params['prop_pred_dim'][0],
+        activation=params['prop_pred_activation'][0],
+        name='prop_pred0'
+    )(prop_input)
+    if params['num_prop_layer'] > 1:
+        for i in range(1, params['num_prop_layer']):
+            x = layers.Dense(
+                units=params['prop_pred_dim'][i],
+                activation=params['prop_pred_activation'][i],
+                name='prop_pred{}'.format(i)
+            )(x)
+    prop_pred_model = Model(prop_input, x, name='prop_pred')
+    return prop_pred_model
